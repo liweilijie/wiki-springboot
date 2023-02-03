961d13bef37dfda3bfc81e47e842a0b3b071b0ba
liwei <liweilijie@gmail.com>
2023-02-03 15:35:42 +0800

    第一次从 start.spring.io 下载2.7.8的代码。

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # wiki-springboot
+
+[start.spring.io](start.sping.io) 选择 web 和 devtools
+热部署两个依赖即可。
---
+++ src/test/java/com/liwei/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liwei.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
