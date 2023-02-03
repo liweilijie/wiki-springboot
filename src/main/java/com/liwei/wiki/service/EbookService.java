33cdfdf2a3026252299e055786dfb67bf09c4101
liwei <liweilijie@gmail.com>
2023-02-03 22:53:16 +0800

    封装请求参数和返回参数.

--- src/main/java/com/liwei/wiki/service/EbookService.java
+++ src/main/java/com/liwei/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.liwei.wiki.service;
 
 import com.liwei.wiki.domain.Ebook;
+import com.liwei.wiki.domain.EbookExample;
 import com.liwei.wiki.mapper.EbookMapper;
+import com.liwei.wiki.req.EbookReq;
+import com.liwei.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service // 为了让 Spring 扫描到这个类
@@ -12,7 +17,19 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list() {
-       return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook: ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
