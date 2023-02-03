eaf04d61106df680325197cbaa378f35383b683b
liwei <liweilijie@gmail.com>
2023-02-03 16:33:36 +0800

    增加 hello 接口测试 test.http, 有get 和 post 两种方式。

--- README.md
+++ README.md
@@ -11,7 +11,20 @@ public static void main(String[] args) { }
 ```
 **sout** 生成代码：`System.out.println();`
 
+用 idea 自带的 http 测试功能来测试，不用移动窗口方便。
+
+**gtr, gtrp, ptr, ptrp** 生成代码：
+```http
+GET http://127.0.0.1:8880/hello
+Accept: application/json
+
+###
+```
+
+
+
 自定义 **live template**:
+
 **logf** 然后生成代码为：`private static final Logger LOG = LoggerFactory.getLogger($CLASSNAME$.class);` 将`CLASSNAME` 映射到 `classname()`即可。
 
 ### log
--- src/main/java/com/liwei/wiki/controller/TestController.java
+++ src/main/java/com/liwei/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.liwei.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // @Controller 返回一个页面
@@ -9,5 +10,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+       return "Hello World! post, " + name;
     }
 }
