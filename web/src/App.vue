17ad6cfb0b8389bd400ce8d30199b3a0db255159
liwei <liweilijie@gmail.com>
2023-02-04 22:26:59 +0800

    制作TheFooter组件.

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
   <a-layout>
     <TheHeader />
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      李伟电子书.
-    </a-layout-footer>
+    <TheFooter></TheFooter>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/TheHeader.vue'; // @ is an alias to /src
+import TheFooter from '@/components/TheFooter.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
+    TheFooter
   },
 });
 </script>
---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    李伟电子书.
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TheFooter',
+});
+</script>
