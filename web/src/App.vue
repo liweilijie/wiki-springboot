55a31f9e00b6aa2e91fd9e45fc3dd5a31f84a1a8
liwei <liweilijie@gmail.com>
2023-02-04 22:23:46 +0800

    制作TheHeader组件.

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <TheHeader />
     <router-view/>
     <a-layout-footer style="text-align: center">
       李伟电子书.
     </a-layout-footer>
   </a-layout>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from '@/components/TheHeader.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheHeader,
+  },
+});
+</script>
 
 <style>
 #components-layout-demo-top-side-2 .logo {
