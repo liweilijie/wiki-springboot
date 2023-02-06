987953ab4ffbc3b15cb55e8d295f786d864bcebb
liwei <liweilijie@gmail.com>
2023-02-06 21:28:30 +0800

    category的父分类改下拉选择.

--- web/src/views/admin/adminCategory.vue
+++ web/src/views/admin/adminCategory.vue
@@ -68,7 +68,17 @@
         <a-input v-model:value="category.name" />
       </a-form-item>
       <a-form-item label="父分类">
-        <a-input v-model:value="category.parent" />
+        <a-select
+            ref="select"
+            v-model:value="category.parent"
+        >
+          <a-select-option value="0">
+            无
+          </a-select-option>
+          <a-select-option v-for="c in level1" :value="c.id" :key="c.id" :disabled="category.id === c.id" >
+           {{ c.name }}
+          </a-select-option>
+        </a-select>
       </a-form-item>
       <a-form-item label="排序">
         <a-input v-model:value="category.sort" />
