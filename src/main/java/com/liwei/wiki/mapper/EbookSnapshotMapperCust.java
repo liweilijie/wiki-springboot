6c1820430d0f0747f9d09e263d632a6588b79697
liwei <liweilijie@gmail.com>
2023-02-13 11:44:35 +0800

    后端定时更新快照功能完成。

---
+++ src/main/java/com/liwei/wiki/mapper/EbookSnapshotMapperCust.java
@@ -0,0 +1,14 @@
+package com.liwei.wiki.mapper;
+
+import com.liwei.wiki.resp.StatisticResp;
+
+import java.util.List;
+
+public interface EbookSnapshotMapperCust {
+
+    public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
+
+    List<StatisticResp> get30Statistic();
+}
